18ce7fa71add847f1a031401825f9e33e2da6667
三島明恵 <myunghye.kor@gmail.com>
2019-07-24 21:43:37 +0900

    data呼び出し

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    index
+    <!-- <img src="/test.jpg" /> -->
+    <div v-for="item in data" :key="item.title">
+      <img :src="item.src" :alt="item.title">
+      <div>{{ item.src }}</div> 
+      <div>{{ item.title }}</div>
+      <div>{{ item.startat }}</div>
+      <div>{{ item.endat }}</div>
+      <div>{{ item.status }}</div>
+      <div>{{ item.place }}</div>
+    </div>
+    {{ data }}
   </div>
 </template>
 
@@ -8,6 +18,18 @@
 export default {
   head: {
     title: 'index'
+  },
+  computed: {
+    data() {
+      return [{
+        src: '/img.jpg',
+        title: '祭りの名前',
+        startat: '2019-07-24',
+        endat: '2019-07-24',
+        status: 'open',
+        place: '川崎'
+      }]
+    }
   }  
 }
 </script>
---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+export const state = () => ({
+    data: [
+        {
+            src: '/img.jpg',
+            title: '祭りの名前',
+            startat: '2019-07-24',
+            endat: '2019-07-24',
+            status: 'open',
+            place: '川崎'
+        },
+        {
+            src: '/img.jpg',
+            title: '祭りの名前',
+            startat: '2019-07-24',
+            endat: '2019-07-24',
+            status: 'open',
+            place: '川崎'
+        }
+    ]
+})
+
+export const getters = {}
+
+export const mutations = {
+}
+
+export const actions = {
+
+}
