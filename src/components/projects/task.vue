3d62fbea21640fc6bca1fd1ad52632063af5d39f
三島明恵 <myunghye.kor@gmail.com>
2019-07-26 20:08:24 +0900

    props/emmit実装

---
+++ src/components/projects/task.vue
@@ -0,0 +1,32 @@
+<template>
+    <div v-show="show">
+        <input type="text" name="" id="" value="task名">
+        <div>
+            <h3>メンバー</h3>
+            <button>add</button>
+        </div>
+        <div>
+            <h3>説明</h3>
+            <textarea name="説明" id="" cols="30" rows="10" placeholder="ここに説明を入れてください。"></textarea>
+        </div>
+        <div>
+            <button @click="close">close</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        show: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        close() {
+            this.$emit("emitClose")
+        }
+    }
+}
+</script>
+
--- src/pages/item/demo.vue
+++ src/pages/item/demo.vue
@@ -1,5 +1,42 @@
 <template>
     <div>
-        demotest
+        <h1><img src="" alt="">川崎ハロウィン</h1>
+        <div>
+           <h2>task</h2>
+           <div>
+               <h3>todo</h3>
+               <ul>
+                   <li @click="toggle">testtask1</li>
+                   <li>testtask2</li>
+                   <li>testtask3</li>
+               </ul>
+               <button>plusButton</button>
+           </div>
+        </div>
+        <div>
+            <h2>member</h2>
+        </div>
+        <div>
+            <h2>mtg</h2>
+        </div>
+        <Projects :show="show" @emitClose="show=false"/>
     </div>
 </template>
+<script>
+import Projects from '@/components/projects/task.vue'
+export default {
+    data() {
+        return {
+            show: false
+        }
+    },
+    components: {
+        Projects
+    },
+    methods: {
+        toggle() {
+            this.show = !this.show 
+        }
+    }
+}
+</script>
